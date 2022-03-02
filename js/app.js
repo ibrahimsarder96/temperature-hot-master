a3d4e9691180e9bed1620cf70648adaf4701238a
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-03-02 18:08:52 +0600

    end commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let API_KEY = `3d806d02199db103c36bf9f5e3f2fe94`
 
 let searchTemperature = () => {
-  const city = document.getElementById('city-name').value;
+  const city = document.getElementById('city-name').value.toLowerCase();
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
   fetch (url)
   .then(res => res.json())
